b3cd5b5284c46b14a40e092e723da18e7797d368
jejn <jimnording@gmail.com>
2015-12-23 15:52:39 +0400

    Add TableView, tableView methods in TableViewController and DetailedViewController

--- MemeMe2/TableViewController.swift
+++ MemeMe2/TableViewController.swift
@@ -11,4 +11,37 @@ import UIKit
 
 class TableViewController: UIViewController {
     
+    // Access to memes array in AppDelegate.swift
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    // Get number of rows from memes Array
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return appDelegate.memes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("MemeTableCell")! as UITableViewCell
+        let meme = appDelegate.memes[indexPath.row]
+        
+        cell.imageView?.image = meme.memedImage
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let meme = appDelegate.memes[indexPath.row]
+        let detailedController = storyboard?.instantiateViewControllerWithIdentifier("DetailedVC") as! DetailedViewController
+        
+        detailedController.meme = meme.memedImage
+        
+        navigationController?.pushViewController(detailedController, animated: true)
+    }
+    
+    
 }
