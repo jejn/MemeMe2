8ae2b4303acc34471672735ba86fa33f5dcbd667
jejn <jimnording@gmail.com>
2015-12-23 11:55:35 +0400

    Add Tab Bar Controller, Navigation Controllers and View Controllers for basic navigation layout.

---
+++ MemeMe2/CollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewController.swift
+//  MemeMe2
+//
+//  Created by Jim Nording on 23/12/15.
+//  Copyright © 2015 Jim Nording. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UIViewController {
+    
+}
