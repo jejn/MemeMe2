8ab4b3041f0a6eb6bbf4cd307d31041f36be8b28
jejn <jimnording@gmail.com>
2015-12-23 11:26:32 +0400

    Initial Commit

---
+++ MemeMe2Tests/MemeMe2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MemeMe2Tests.swift
+//  MemeMe2Tests
+//
+//  Created by Jim Nording on 23/12/15.
+//  Copyright © 2015 Jim Nording. All rights reserved.
+//
+
+import XCTest
+@testable import MemeMe2
+
+class MemeMe2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
