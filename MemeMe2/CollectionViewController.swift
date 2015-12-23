55b5ec83e38f827dd2b10b751336a71fd121417e
jejn <jimnording@gmail.com>
2015-12-23 23:20:43 +0400

    Add CollectionViewController methods and Custom Cell for Collections. Needs more work.

--- MemeMe2/CollectionViewController.swift
+++ MemeMe2/CollectionViewController.swift
@@ -9,6 +9,60 @@
 import Foundation
 import UIKit
 
-class CollectionViewController: UIViewController {
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    
+    // Access to memes array in AppDelegate.swift
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let space: CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+    }
+    
+    
+    
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return appDelegate.memes.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MemeCollectionViewCell", forIndexPath: indexPath) as! MemeCollectionViewCell
+        
+        let meme = appDelegate.memes[indexPath.row]
+        
+        cell.memeView?.image = meme.memedImage
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, canFocusItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let meme = appDelegate.memes[indexPath.row]
+        let detailedController = storyboard?.instantiateViewControllerWithIdentifier("DetailedVC") as! DetailedViewController
+        
+        detailedController.meme = meme
+        
+        navigationController?.pushViewController(detailedController, animated: true)
+    }
     
 }
---
+++ MemeMe2/MemeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe2
+//
+//  Created by Jim Nording on 23/12/15.
+//  Copyright © 2015 Jim Nording. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeView: UIImageView!
+    
+    
+}
